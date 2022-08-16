fad38bba355ed05418b2fe6f4b3457ce2bdc4149
Neemahj~ <neemahjinadu@gmail.com~>
2022-08-16 19:19:31 +0100

    Profile page commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import SignUp from "./Components/SignUpPage/SignUp";
-import { useState } from "react";
 import SideBar from "./Components/NavBar/SideBar";
 import Feeds from "./Components/Feeds/Feeds";
 import Widget from "./Components/Widget/Widget";
@@ -8,24 +7,19 @@ import Profile from "./Components/Profile/Profile";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
- 
-
   return (
-    <> 
-    <SignUp />
-     
-     {/* <div className="app">
-      <SideBar />
-      <Routes>
-        <Route path="/" element={<Feeds/>}/> 
-        <Route path="/profile" element={<Profile/>}/>
-      </Routes>
-      <Widget />
-    </div> */}
+    <>
+      {/* <SignUp /> */}
 
+      <div className="app">
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<Feeds />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+        <Widget />
+      </div>
     </>
-
-   
   );
 }
 
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import "./Profile.css";
+import { HiArrowSmLeft, HiOutlineLocationMarker } from "react-icons/hi";
+import { RiGlobeLine } from "react-icons/ri";
+import { BsCalendar3 } from "react-icons/bs";
 
 const Profile = () => {
   return (
-    <div>
-      profile
-    </div>
-  )
-}
+    <div className="profile-page">
+      <header className="profile-header">
+        <HiArrowSmLeft style={{ fontSize: 25 }} />
+        <div className="profile-htext">
+          <p className="profile-hname">Neemah🌹</p>
+          <p className="profile-tweets-no">3,939 Tweets</p>
+        </div>
+      </header>
 
-export default Profile
+      {/* profile images */}
+      <div className="profile-images">
+        <div className="profile-cover-photo">
+          <img
+            alt="cover-photo"
+            src="https://tse3.explicit.bing.net/th?id=OIP.tjp_pimao0qJDBt4WQKqbAHaEK&pid=Api&P=0"
+          />
+        </div>
+        <div className="profile-dp">
+          <img
+            alt="Neemah🌹"
+            src="https://pbs.twimg.com/profile_images/1493892914908389377/GnyIwffF_normal.jpg"
+          />
+        </div>
+        <button className="profile-edit-button">Edit Profile</button>
+      </div>
+
+      <div>
+        <p>Neemah🌹</p>
+        <p>@neemahj_</p>
+
+        <p>still working on my bio!</p>
+      </div>
+
+      <HiOutlineLocationMarker />
+      <RiGlobeLine />
+      <BsCalendar3 />
+
+    </div>
+  );
+};
+
+export default Profile;
